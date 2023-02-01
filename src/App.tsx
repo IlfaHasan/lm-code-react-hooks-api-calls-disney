2b66af35e65f92d3e25e4f92f14187645ca3cec4
ilfa <ilfahassan@gmail.com>
2023-02-01 22:45:37 +0000

    Activity 5 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,48 @@
-import "./App.css";
-import React, { useEffect, useState } from "react";
-import Header from "./components/header";
-import CharacterContainer from "./components/character_container";
-import Navigation from "./components/navigation";
-import { DisneyCharacter } from "./disney_character";
+import './App.css';
+import React, { useState } from 'react';
+import Header from './components/header';
+import CharacterContainer from './components/character_container';
+import Navigation from './components/navigation';
+import { DisneyCharacter } from './disney_character';
+import axios from 'axios';
+import { useEffect } from 'react';
 
-const App: React.FC = () => {
-  const [currentPage, setCurrentPage] = useState<number>(1);
+export const FavouritesContext = React.createContext<number[]>( []);
+
+const App : React.FC = () => {
+
+	const [currentPage, setCurrentPage] = useState<number>(1);
+	const [characterFavourites, setCharacterFavourites] = useState<Array<number>>([]);
 
   // Some dummy state representing disney characters
-  const [characters, setCharacters] = useState<Array<DisneyCharacter>>([]);
-  const getCharacters = async (pageNumber: number) => {
-    const apiResponse = await fetch(
-      `http://api.disneyapi.dev/characters?page=${pageNumber}`
-    );
-    const json = (await apiResponse.json()) as { data: DisneyCharacter[] };
-    setCharacters(json.data);
-  };
+  const [characters, setCharacters] = useState<Array<DisneyCharacter>>([
 
-  const [characterFavourites,setCharacterFavourites]=useState<any>([]);
+  ]);
 
   useEffect(() => {
-    getCharacters(currentPage);
+	getCharacters(currentPage);
   }, [currentPage]);
 
-  return (
-    <div className="page">
-      <Header currentPage={currentPage} />
-      <Navigation currentPage={currentPage} setCurrentPage={setCurrentPage} />
-      <CharacterContainer characters={characters}
-                    characterFavourites={characterFavourites}
-                    updateFavourites={setCharacterFavourites}  />
-    </div>
-  );
-};
+  const getCharacters = async (pageNumber : number) => {
+	// Utilised Axios for API calls
+	const apiResponse = await axios.get(`http://api.disneyapi.dev/characters?page=${pageNumber}`);
+	setCharacters(apiResponse.data.data);
+	console.log(characters);
+  };
+
+
+
+	return (
+		<FavouritesContext.Provider value={characterFavourites}>
+		<div className="page">
+			<Header currentPage={currentPage} />
+			<Navigation currentPage={currentPage} setCurrentPage={setCurrentPage} />
+			<CharacterContainer characters={characters} updateFavourites={setCharacterFavourites}  />
+		</div>
+		</FavouritesContext.Provider>
+	);
+
+}
 
 export default App;
+
--- src/components/character.tsx
+++ src/components/character.tsx
@@ -1,42 +1,38 @@
 import { DisneyCharacter } from "../disney_character"
+import React, { useContext } from 'react';
+import { FavouritesContext } from '../App';
+
 interface CharacterProps{
 	character: DisneyCharacter;
-	characterFavourites: Array<number>;
 	updateFavourites: (favourites: Array<number>) => void;
 }
-// for our props we can reuse the DisneyCharacter interface
-// - defining an anonymous type that just has one property - a DisneyCharacter
-const Character : React.FC<CharacterProps> = ( { character, characterFavourites, updateFavourites }) => {
-let imageSrc = "https://picsum.photos/300/200/?blur";
-  if (character.imageUrl) {
-        imageSrc = character.imageUrl.substring(0, character.imageUrl.indexOf('/revision'));
-  }
-  function toggleFavouriteForCharacter(characterId : number) {
-    if(!characterFavourites.includes(characterId)) {
-        // add to favourites
-        updateFavourites([...characterFavourites, characterId]);
-    }
-    else {
-      // remove from favourites
-      const updatedFavourites = characterFavourites.filter((id) => id !== characterId);
-      updateFavourites(updatedFavourites);
-    }
-  }
-  {return(
-  
-    <article className="character-item">
+
+const Character : React.FC<CharacterProps> = ( { character, updateFavourites }) => {
+
+	const characterFavourites = useContext(FavouritesContext);
+
+	function toggleFavouriteForCharacter(characterId : number) {
+		if(!characterFavourites.includes(characterId)) {
+			updateFavourites([...characterFavourites, characterId]);
+		}
+		else {
+		  const updatedFavourites = characterFavourites.filter((id) => id !== characterId);
+		  updateFavourites(updatedFavourites);
+		}
+	  }
+
+    return(<article className="character-item">
 
       <h2>{character.name}</h2>
 
-      <div className="character-item__actions" onClick={() => toggleFavouriteForCharacter(character._id)}>
-  {!characterFavourites.includes(character._id) ? "Add to Favourites" : "Favourited"}
-</div>
+      <div className="character-item__actions"  onClick={() => toggleFavouriteForCharacter(character._id)}>
+	  	{!characterFavourites.includes(character._id) ? "Add to Favourites" : "Favourited"}
+      </div>
 
       <img className="character-item__img" src={character.imageUrl} alt={character.name} />
 
-    </article>
-    )
-  }
+    </article>);
 }
 
-export default Character
+
+export default Character;
